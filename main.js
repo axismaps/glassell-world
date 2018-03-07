f6fa0392e9a5f32749239b66edb51f057cdcde5c
David Heyman <dave@axismaps.com>
2018-03-07 14:19:30 +0000

    Hard linting

--- main.js
+++ main.js
@@ -1,50 +1,56 @@
-var codesByName = {};
+/* globals L */
+/* globals topojson */
+/* globals codes */
+/* globals countriesData */
+
+let codesByName = {};
 Object.keys(codes).forEach(function (c) {
   codesByName[codes[c]] = c;
 });
 
-var initCenter = [20, 0];
-var initZoom = 3;
+let initCenter = [20, 0];
+let initZoom = 3;
+let resetTimer;
 
-var map = L.map('map', {zoomControl: false}).setView(initCenter, initZoom);
+let map = L.map('map', { zoomControl: false }).setView(initCenter, initZoom);
 
-var reset = L.control({ position: 'bottomright' });
-reset.onAdd = function(map) {
-  var div = document.createElement('div');
+let reset = L.control({ position: 'bottomright' });
+reset.onAdd = function () {
+  let div = document.createElement('div');
   div.className = 'reset';
   div.onclick = function () {
     map.setView([20, 0], 3);
   };
   return div;
-}
+};
 reset.addTo(map);
 
-L.control.zoom({position: 'bottomright'}).addTo(map);
+L.control.zoom({ position: 'bottomright' }).addTo(map);
 
 
-var tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
   maxZoom: 16
 }).addTo(map);
 
-var geojson = topojson.feature(countriesData, countriesData.objects.countries);
+let geojson = topojson.feature(countriesData, countriesData.objects.countries);
 
-var style = {
+let style = {
   fillOpacity: 0,
   stroke: false,
   color: '#EB6E2E',
   weight: 6
-}
+};
 
-var highlightedFeature;
+let highlightedFeature;
 
 L.geoJSON(geojson, {
-  style: function (){ return style; }
+  style: function () { return style; }
 }).on('click', function (e) {
-  document.getElementById('prompt').style.display = 'none';
+  // document.getElementById('prompt').style.display = 'none';
   clearTimeout(resetTimer);
   resetTimer = setTimeout(resetAll, 60000);
-  if (highlightedFeature != e.layer) {
+  if (highlightedFeature !== e.layer) {
     highlightFeature(e.layer);
     showProbe(e.layer.feature, e.containerPoint);
   } else {
@@ -56,8 +62,8 @@ map.on('zoom', function () {
   removeHighlight();
   hideProbe();
 }).on('click', function (e) {
-  var target = e.originalEvent.target;
-  if (!target || target.tagName.toLowerCase() != 'path') {
+  let { target } = e.originalEvent;
+  if (!target || target.tagName.toLowerCase() !== 'path') {
     removeHighlight();
     hideProbe();
   }
@@ -66,46 +72,45 @@ map.on('zoom', function () {
   resetTimer = setTimeout(resetAll, 60000);
 });
 
-function highlightFeature (layer) {
+function highlightFeature(layer) {
   removeHighlight();
-  layer.setStyle({stroke: true});
+  layer.setStyle({ stroke: true });
   highlightedFeature = layer;
 }
 
-function removeHighlight () {
-  if (highlightedFeature) highlightedFeature.setStyle({stroke: false});
+function removeHighlight() {
+  if (highlightedFeature) highlightedFeature.setStyle({ stroke: false });
   highlightedFeature = null;
   hideProbe();
 }
 
-function showProbe (feature, point) {
-  var probe = document.getElementById('probe');
-  var iso2;
-  if (feature.properties.ISO_A2 == -99) {
+function showProbe(feature, point) {
+  let probe = document.getElementById('probe');
+  let iso2;
+  if (feature.properties.ISO_A2 === '-99') {
     iso2 = codesByName[feature.properties.NAME];
   } else {
     iso2 = feature.properties.ISO_A2;
   }
-  var img = iso2 ? ('<img src="flags/' + iso2.toLowerCase() + '.png"/>') : ''
-  probe.innerHTML = img + '<span class="country-name">' + feature.properties.NAME + '</span><br>' + 
+  let img = iso2 ? ('<img src="flags/' + iso2.toLowerCase() + '.png"/>') : '';
+  probe.innerHTML = img + '<span class="country-name">' + feature.properties.NAME + '</span><br>' +
     'Population: ' + feature.properties.POP_EST.toLocaleString() + '<br>' +
-    'GDP: US $' + feature.properties.GDP_MD_EST.toLocaleString() + '<br>' + 
+    'GDP: US $' + feature.properties.GDP_MD_EST.toLocaleString() + '<br>' +
     'Income Group: ' + feature.properties.INCOME_GRP.slice(3).replace(' income', '').replace(': OECD', '');
   probe.style.display = 'block';
-  var w = probe.offsetWidth;
-  var h = probe.offsetHeight;
-  var x = point.x > window.innerWidth / 2 ? (point.x - w - 10) : (point.x + 10);
-  var y = point.y > 200 ? (point.y - h - 10) : (point.y + 10);
+  let w = probe.offsetWidth;
+  let h = probe.offsetHeight;
+  let x = point.x > window.innerWidth / 2 ? (point.x - w - 10) : (point.x + 10);
+  let y = point.y > 200 ? (point.y - h - 10) : (point.y + 10);
   probe.style.left = x + 'px';
   probe.style.top = y + 'px';
 }
 
-function hideProbe () {
+function hideProbe() {
   document.getElementById('probe').style.display = 'none';
 }
 
-var resetTimer;
-function resetAll () {
-  document.getElementById('prompt').style.display = 'block';
+function resetAll() {
+  // document.getElementById('prompt').style.display = 'block';
   map.setView(initCenter, initZoom);
 }
