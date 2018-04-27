190a07419ad9f139071587313390cc38df9cee2c
David Heyman <dave@axismaps.com>
2018-04-27 10:51:22 +0100

    Retina tiles and min / max zoom

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ L.control.zoom({ position: 'bottomright' }).addTo(map);
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
   maxZoom: 16,
-  minZoom: 1
+  minZoom: 2,
+  detectRetina: true
 }).addTo(map);
 
 let geojson = topojson.feature(countriesData, countriesData.objects.countries);
